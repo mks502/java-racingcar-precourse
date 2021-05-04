3f7f16cc3120c66c7e03ec2f973655e4878df2e5
MaKyuSeok <mks502@naver.com>
2021-05-04 13:09:16 +0900

    refactor(Car) : Car 리팩토링
    메소드 분리를 통한 Car 리팩토링
    하나의 메소드가 하나의 역할을 분명히 하도록 하였습니다.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -26,13 +26,22 @@ public class Car {
     }
 
     public void controlMove(int number) {
+        isValidNumber(number);
+
+        if (isAvailableMove(number)) {
+            moveFoward();
+        }
+    }
+
+    private boolean isValidNumber(int number){
         if (number < MIN_RANGE_NUM || number > MAX_RANGE_NUM) {
             throw new IllegalArgumentException("값의 범위를 벗어났습니다.");
         }
-        if (number >= MOVE_FORWARD_CONDITION_NUMBER) {
-            moveFoward();
-            return;
-        }
+        return true;
+    }
+
+    private boolean isAvailableMove(int number){
+        return number >= MOVE_FORWARD_CONDITION_NUMBER;
     }
 
     public void play() {
